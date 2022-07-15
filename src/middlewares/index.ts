a9a04f9a64a60945524245c26371eba3ffdcb390
elefantinhos2 <douglasramires01@gmail.com>
2022-07-15 02:35:52 -0300

    feat: middlewareAuthentication

---
+++ src/middlewares/index.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken"
+import "dotenv/config"
+
+export const AuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+
+    const token = req.headers.authorization
+
+    if(!token){
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+    }
+
+    const splitToken = token.split(" ")
+
+    jwt.verify(splitToken[1], process.env.SECRET_KEY as string, (error: any, decoded: any) => {
+        if(error){
+            return res.status(401).json({
+                message: "Invalid token"
+            })
+        }
+
+        req.data = {
+            id: decoded.id,
+            adm: decoded.adm
+        }
+
+        next()
+
+    })
+
+}
+
