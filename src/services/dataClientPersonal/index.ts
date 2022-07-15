6320d35203c3874717edec613e35835224c3fc24
elefantinhos2 <douglasramires01@gmail.com>
2022-07-15 01:33:26 -0300

    feat: Criando Service do CreateDataClientsPersonal

---
+++ src/services/dataClientPersonal/index.ts
@@ -0,0 +1,51 @@
+import { IDataRequest, IDataResponse } from "../../interfaces/data";
+import  AppDataSource  from "../../data-source";
+import { DataClientPersonal } from "../../entities/dataClientPersonal.entities";
+
+
+export const createDataService = async ({name, email, age, password, phone_number, adm, plan, checkin, checkout, lock_number}: IDataRequest) => {
+
+    const userRepository = AppDataSource.getRepository(DataClientPersonal) 
+
+    const users = await userRepository.find()
+
+    //verificação de enaiul já cadastrado 
+    const emailAlreadyExisty = users.find(user => user.email ===email) 
+
+    //se o email for repetido forçamos um  erro
+    if( emailAlreadyExisty ) {
+        throw new Error("Email already existy")
+    }
+
+    //usando os parametros que vamos receber lá do controller
+    const data = new DataClientPersonal()
+    data.name = name
+    data.email = email
+    data.age = age
+    data.password = password
+    data.phone_number = phone_number
+    data.adm = adm
+    data.status = true
+    data.plan = plan
+    data.checkin = checkin
+    data.checkout = checkout
+    data.lock_number = lock_number
+
+    //adionando ao DB
+    userRepository.create(data)
+    await userRepository.save(data)
+    
+    //Criando uma resposta com chanes especificas
+    const userResponse:IDataResponse = {
+        id: data.id,
+        name,
+        email,
+        age,
+        plan,
+        status: data.status,
+        lock_number
+    }
+
+    return userResponse;
+
+}
