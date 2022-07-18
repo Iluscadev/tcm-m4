6caef616576c74efa9754be6709f11eb4be6b626
matheusprado1 <matheusprado1096@gmail.com>
2022-07-18 15:41:44 -0300

    feat: sinc developer

--- src/entities/dataClientPersonal.entities.ts
+++ src/entities/dataClientPersonal.entities.ts
@@ -1,53 +1,71 @@
-import { Entity, Column, PrimaryColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinTable,
+  ManyToMany,
+} from "typeorm";
 import { v4 as uuid } from "uuid";
+import Address from "./address.entity";
+import Journal from "./journal.entity";
 
-@Entity()
-export class DataClientPersonal{
-    @PrimaryColumn("uuid")
-    readonly id: string
+@Entity("data_client_personal")
+export class DataClientPersonal {
+  @PrimaryColumn("uuid")
+  readonly id: string;
 
-    @Column()
-    name: string
+  @Column()
+  name: string;
 
-    @Column()
-    email: string
+  @Column()
+  email: string;
 
-    @Column()
-    age: string
+  @Column()
+  age: string;
 
-    @Column()
-    password: string
+  @Column()
+  password: string;
 
-    @Column()
-    phone_number: string
+  @Column()
+  phone_number: string;
 
-    @Column()
-    status: boolean
+  @Column()
+  status: boolean;
 
-    @Column()
-    adm: boolean
+  @Column()
+  adm: boolean;
 
-    @Column()
-    plan: string
+  @Column()
+  plan: string;
 
-    @Column()
-    checkin: string
+  @Column()
+  checkin: string;
 
-    @Column()
-    checkout: string
+  @Column()
+  checkout: string;
 
-    @Column()
-    lock_number: number
+  @Column()
+  lock_number: number;
 
-    @CreateDateColumn()
-    create_at: Date
+  @CreateDateColumn()
+  created_at: Date;
 
-    @UpdateDateColumn()
-    update_at: Date
+  @UpdateDateColumn()
+  updated_at: Date;
 
-    constructor(){
-        if(!this.id){
-            this.id = uuid()
-        }
+  @ManyToMany(() => Address, (address) => address.data_client_personal)
+  @JoinTable()
+  addresses: Address[];
+
+  @ManyToMany(() => Journal, (journal) => journal.data_client_personal)
+  @JoinTable()
+  journals: Journal[];
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
     }
+  }
 }
