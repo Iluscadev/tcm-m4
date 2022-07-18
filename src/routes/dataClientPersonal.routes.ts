c277ef98f3d710d6c6f64f278ae323b435cfdd64
alexandre <alexandrelima2021@outlook.com>
2022-07-18 18:33:40 -0300

    fix: route login

--- src/routes/dataClientPersonal.routes.ts
+++ src/routes/dataClientPersonal.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { createDataController, ListAllController, userListOneController } from "../controllers/dataClientPersonal";
+import { userLoginController } from "../controllers/login";
 
 const clientsPersonalRoutes = Router()
+
+clientsPersonalRoutes.post('/login', userLoginController);
 
 clientsPersonalRoutes.post('/register', createDataController);
 
