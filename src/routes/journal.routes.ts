decbcfd3d3b454ab8ade67880fed07e2b1570928
elefantinhos2 <douglasramires01@gmail.com>
2022-07-20 04:06:50 -0300

    fix: createCRUD_Journal

--- src/routes/journal.routes.ts
+++ src/routes/journal.routes.ts
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { journalCreateController, journalListController, journalUpdateController } from "../controllers/journals";
 
 const journalRoutes = Router();
+
+journalRoutes.post('/journal/:id', journalCreateController);
+
+journalRoutes.get('/journal/list', journalListController)
+
+journalRoutes.patch('/journal/:id', journalUpdateController)
 
 export default journalRoutes;
