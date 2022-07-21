0d1cd7e0bcf85521ebc4c5ddbe93d648b69ac8ad
Helashow <lucas.bnlsp2013@gmail.com>
2022-07-20 23:38:41 -0300

    resolved remaining conflict

--- src/services/dataClientPersonal/index.ts
+++ src/services/dataClientPersonal/index.ts
@@ -1,12 +1,8 @@
 import { DataClientPersonal } from "../../entities/dataClientPersonal.entity";
 import AppDataSource from "../../data-source";
 import { IDataRequest, IDataResponse } from "../../interfaces/data";
-<<<<<<< HEAD
-import { hash } from "bcryptjs"
-=======
 import { hash } from "bcryptjs";
 import { AppError } from "../../errors/AppError";
->>>>>>> developer
 
 export const ListAllService = async () => {
   const userRepository = AppDataSource.getRepository(DataClientPersonal);
@@ -93,6 +89,7 @@ export const createDataService = async ({
     name,
     email,
     age,
+    plan,
     status: data.status
   };
 
@@ -158,67 +155,11 @@ export const softDeleteService = async (id: string) => {
         throw new AppError("User not found", 404)
     }
 
-<<<<<<< HEAD
-    return user;
-
-}
-
-
-export const createDataService = async ({name, email, age, password, phone_number, adm, plan, checkin, checkout, lock_number}: IDataRequest) => {
-
-    const userRepository = AppDataSource.getRepository(DataClientPersonal) 
-
-    const users = await userRepository.find()
-
-    const hashedPassword = await hash(password, 10)
-
-    //verificação de enaiul já cadastrado 
-    const emailAlreadyExisty = users.find(user => user.email ===email) 
-
-    //se o email for repetido forçamos um  erro
-    if( emailAlreadyExisty ) {
-        throw new Error("Email already existy")
-    }
-
-    //usando os parametros que vamos receber lá do controller
-    const data = new DataClientPersonal()
-    data.name = name
-    data.email = email
-    data.age = age
-    data.password = hashedPassword
-    data.phone_number = phone_number
-    data.adm = adm
-    data.status = true
-    data.plan = plan
-    data.checkin = checkin
-    data.checkout = checkout
-    data.lock_number = lock_number
-
-    //adionando ao DB
-    userRepository.create(data)
-    await userRepository.save(data)
-    
-    //Criando uma resposta com chanes especificas
-    const dataResponse:IDataResponse = {
-        id: data.id,
-        name,
-        email,
-        age,
-        plan,
-        status: data.status,
-        lock_number
-    }
-
-    return dataResponse;
-
-}
-=======
     if(!user.status){
         throw new AppError("Inactive user")
     }
 
     user.status = false
     await userRepository.save(user)
->>>>>>> developer
 
 }
